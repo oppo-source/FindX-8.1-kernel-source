f165820c083c64aac1af1e9b6ba26379ccbc6fa6
snowwolf725 <snowwolf725@gmail.com>
2019-02-09 17:15:22 +0000

    Fix compile errors

--- drivers/power/oppo/charger_ic/oppo_battery_sdm845.h
+++ drivers/power/oppo/charger_ic/oppo_battery_sdm845.h
@@ -20,10 +20,10 @@
 #include <linux/regulator/driver.h>
 #include <linux/regulator/consumer.h>
 #include <linux/extcon.h>
-#include "../../../../kernel/msm-4.9/drivers/power/supply/qcom/storm-watch.h"
-#include "../../../../kernel/msm-4.9/drivers/power/supply/qcom/smb-reg.h"
-#include "../../../../kernel/msm-4.9/drivers/power/supply/qcom/battery.h"
-#include "../../../../kernel/msm-4.9/drivers/power/supply/qcom/step-chg-jeita.h"
+#include "../../supply/qcom/storm-watch.h"
+#include "../../supply/qcom/smb-reg.h"
+#include "../../supply/qcom/battery.h"
+#include "../../supply/qcom/step-chg-jeita.h"
 
 
 enum print_reason {
